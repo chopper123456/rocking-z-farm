713439d53477cd4b12630ce2c31c37aad00f675e
chopper123456 <cademcneil99@gmail.com>
2026-01-29 19:39:29 -0700

    Load env vars first

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
